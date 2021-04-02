b994bde57acef4a02e74b9867be75f5a355a622a
Guilherme Carneiro <guilherme.carneiro@splab.ufcg.edu.br>
2021-04-02 14:12:02 -0300

    first commit

---
+++ src/main/java/com/rc/redacaoconectada/RedacaoconectadaApplication.java
@@ -0,0 +1,13 @@
+package com.rc.redacaoconectada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedacaoconectadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedacaoconectadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rc/redacaoconectada/RedacaoconectadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.redacaoconectada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedacaoconectadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
