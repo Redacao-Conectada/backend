265ac9137379728400c7ab57cee88015ece1c4f6
Artur Souza <artur.souza@ccc.ufcg.edu.br>
2021-05-04 17:03:37 -0300

    ajeitando os problemas do mergeRequest

--- src/main/java/com/rc/redacaoconectada/services/UserService.java
+++ src/main/java/com/rc/redacaoconectada/services/UserService.java
@@ -30,7 +30,7 @@ public class UserService implements UserDetailsService {
 
   @Autowired
   private RoleRepository roleRepository;
-  
+
   @Autowired
   private ChangeRoleRequestRepository changeRoleRequestRepository;
 
