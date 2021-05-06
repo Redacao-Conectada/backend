729d41dbf69ced1ba06904f50a86a2a9ea43ecc0
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2021-05-06 20:05:17 -0300

    add admin route config

--- src/test/java/com/rc/redacaoconectada/factory/EssayFactory.java
+++ src/test/java/com/rc/redacaoconectada/factory/EssayFactory.java
@@ -9,7 +9,7 @@ public class EssayFactory {
 
     public static Essay createEssay() {
         User user = UserFactory.createUser();
-        return new Essay(2L, user, null,0, "Redação é o processo de redigir (escrever) um texto.", Instant.now());
+        return new Essay(2L, user, null,0, "Redação é o processo de redigir (escrever) um texto.", Instant.now(), false);
     }
 
 }
