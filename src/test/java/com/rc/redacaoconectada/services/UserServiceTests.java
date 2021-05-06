1f20608588aff7bfbac7c57bebdb011a703e4d11
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2021-05-06 19:31:06 -0300

    RF-30 test

--- src/test/java/com/rc/redacaoconectada/services/UserServiceTests.java
+++ src/test/java/com/rc/redacaoconectada/services/UserServiceTests.java
@@ -1,20 +1,27 @@
 package com.rc.redacaoconectada.services;
 
+import com.rc.redacaoconectada.dtos.UserDTO;
 import com.rc.redacaoconectada.entities.User;
 import com.rc.redacaoconectada.factory.UserFactory;
 import com.rc.redacaoconectada.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 class UserServiceTests {
@@ -28,13 +35,16 @@ class UserServiceTests {
   private User existingUser;
   private String existingUserEmail;
   private String nonExistingUserEmail;
+  private PageImpl<User> page;
 
   @BeforeEach
   void setUp() throws Exception {
     existingUser = UserFactory.createUser();
     existingUserEmail = "bob@gmail.com";
     nonExistingUserEmail = "haha.2332@gmail.com";
+    page = new PageImpl<>(List.of(existingUser));
 
+    when(repository.findUserByName(any(), any())).thenReturn(page);
     when(repository.findByEmail(existingUserEmail)).thenReturn(existingUser);
     doThrow(UsernameNotFoundException.class).when(repository).findByEmail(nonExistingUserEmail);
   }
@@ -49,5 +59,15 @@ class UserServiceTests {
   @Test
   void loadUserByUsernameShouldThrowUsernameNotFoundExceptionWhenEmailDoesNotExists() {
     assertThrows(UsernameNotFoundException.class, () -> service.loadUserByUsername(nonExistingUserEmail));
+  }
+
+  @Test
+  void findAllPagedShouldReturnPage() {
+    PageRequest request = PageRequest.of(0, 12);
+    Page<UserDTO> page = service.findUserEssays("", request);
+
+    Assertions.assertNotNull(page);
+    Assertions.assertEquals(1, page.getTotalElements());
+    verify(repository, times(1)).findUserByName("", request);
   }
 }
